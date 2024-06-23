1e3892653b749c094744f68afbecd398b47b7c90
yomaytk <yoshimura.masashi.frbs@gmail.com>
2024-06-23 23:10:59 +0900

    Add 64bit FLAG NZCV.

--- backend/remill/include/remill/Arch/AArch64/Runtime/State.h
+++ backend/remill/include/remill/Arch/AArch64/Runtime/State.h
@@ -322,12 +322,14 @@ struct alignas(16) AArch64State : public ArchState {
 
   SleighFlagState sleigh_flags;  // 24 bytes.
 
-  uint8_t padding[8];
+  uint64_t sr_nzcv;
+
+  // uint8_t padding[8];
 
 } __attribute__((packed));
 
 static_assert((1200 /* simd ~ _3 */ + 16 /* ArchState */ + 24 /* sleigh_flags */ +
-               8 /* padding */) == sizeof(AArch64State),
+               8 /* sr_nzcv */) == sizeof(AArch64State),
               "Invalid packing of `struct State`");
 
 struct State : public AArch64State {};
