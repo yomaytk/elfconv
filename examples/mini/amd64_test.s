0e07bab1bdf47e858ed430783cedc2b45b72b5fb
yomaytk <yoshimura.masashi.frbs@gmail.com>
2025-02-21 00:23:22 +0900

    Add call test to the amd64_test.s.

--- examples/mini/amd64_test.s
+++ examples/mini/amd64_test.s
@@ -1,5 +1,6 @@
 .intel_syntax noprefix
 .section .data
+
 success_msg: 
     .string "success.\n"
 
@@ -12,10 +13,24 @@ add_gprv_immb_error_msg:
 mov_memv_gprv_error_msg:
     .string "[ERROR] MOV_MEMv_GPRv\n"
 
+call_procedure_error_msg:
+    .string "[ERROR] CALL_NEAR_RELBRd\n"
+
 .section .text
 .globl _start
+
 _start:                  
     jmp test_mov_gprv_immz
+
+func1:
+    push rbp
+    mov rbp, rsp
+    mov qword ptr [rbp - 8], rdi
+    mov qword ptr [rbp - 16], rsi
+    mov rax, qword ptr [rbp - 8]
+    add rax, qword ptr [rbp - 16]
+    pop rbp
+    ret
 
 test_mov_gprv_immz:
     mov rbx, 50   
@@ -25,6 +40,7 @@ test_mov_gprv_immz:
 
 fail_mov_gprv_immz:
     mov rax, 1
+    mov rdi, 1
     lea rsi, [rip + mov_gprv_immz_error_msg]
     mov rdx, 24
     syscall
@@ -39,6 +55,7 @@ test_add_gprv_immb:
 
 fail_add_gprv_immb:
     mov rax, 1
+    mov rdi, 1
     lea rsi, [rip + add_gprv_immb_error_msg]
     mov rdx, 23
     syscall
@@ -51,17 +68,35 @@ test_mov_memv_gprv:
     mov rbx, qword ptr [rbp - 4]
     cmp rbx, 20
     jne fail_mov_memv_gprv
-    jmp success
+    jmp test_call_procedure
     
 fail_mov_memv_gprv:
     mov rax, 1
+    mov rdi, 1
     lea rsi, [rip + mov_memv_gprv_error_msg]
     mov rdx, 23
     syscall
     jmp exit
-    
+
+test_call_procedure:
+    mov rdi, 30
+    mov rsi, 30
+    call func1
+    cmp rax, 60
+    jne fail_call_procedure
+    jmp success
+
+fail_call_procedure:
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rip + call_procedure_error_msg]
+    mov rdx, 25
+    syscall
+    jmp exit
+
 success:
     mov rax, 1
+    mov rdi, 1
     lea rsi, [rip + success_msg]
     mov rdx, 9
     syscall
